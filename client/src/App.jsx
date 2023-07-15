0de5d8d06d900e3ec9ae19762b05dd0e3660bd1c
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-07-15 23:59:00 +0100

    src: react router dom setup in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home'
+import About from './pages/about/About'
+import Projects from './pages/projects/Projects'
+import Contact from './pages/contact/Contact'
+import NotFound from './pages/notFound/NotFound'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Routes>
+            <Route index element={<Home/>}/>
+            <Route path='about' element={<About/>}/>
+            <Route path='projects' element={<Projects/>}/>
+            <Route path='contact' element={<Contact/>}/>
+            <Route path='*' element={<NotFound/>}/>    
+        </Routes>
+    </BrowserRouter>
   )
 }
 

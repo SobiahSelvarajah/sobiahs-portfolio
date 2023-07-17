ac8cbb8303e28e692ac46bf8a847d3f07f49775a
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-07-17 07:37:04 +0100

    background waves coded - clean and ready

---
+++ client/src/components/bgWaves/BgWaves.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './BgWaves.scss'
+
+const BgWaves = () => {
+  return (
+    <div className="bgWaves__container">
+        <div className="bgWaves__wave"></div>
+        <div className="bgWaves__wave"></div>
+    </div>
+  )
+}
+
+export default BgWaves

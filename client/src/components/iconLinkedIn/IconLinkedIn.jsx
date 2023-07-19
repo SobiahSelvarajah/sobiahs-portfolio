5e9a9c8755ed38d72357183f9418a452cf64b826
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-07-19 19:54:37 +0100

    iconLinkedIn ready to push

---
+++ client/src/components/iconLinkedIn/IconLinkedIn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AiFillLinkedin} from 'react-icons/ai'
+
+const IconLinkedIn = () => {
+  return (
+    <a href="https://www.linkedin.com/in/sobiah-selvarajah/" className="iconLinkedIn__link">
+        <button className="iconLinkedIn__button">
+            <AiFillLinkedin/>
+        </button>
+    </a>
+  )
+}
+
+export default IconLinkedIn

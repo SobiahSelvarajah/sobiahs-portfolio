2c23fe2d441e2e4df1065eefb1fa6c9c8899a9ac
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-07-19 19:48:48 +0100

    iconInstagram ready to push

---
+++ client/src/components/iconInstagram/IconInstagram.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AiFillInstagram } from 'react-icons/ai'
+
+const IconInstagram = () => {
+  return (
+    <a href="https://www.instagram.com/sobiah11/" className="iconInstagram__link">
+        <button className="iconInstagram__button">
+            <AiFillInstagram/>
+        </button>
+    </a>
+  )
+}
+
+export default IconInstagram

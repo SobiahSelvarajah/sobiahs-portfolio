c0bc4afb436a947baaba7763cc3ff050c788a01a
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-07-25 19:04:17 +0100

    homeTextRight ready to push

---
+++ client/src/components/homeTextRight/HomeTextRight.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './HomeTextRight.scss'
+
+const HomeTextRight = () => {
+  return (
+    <section className="homeTextRight__container">
+        <h5 className="homeTextRight__text">
+            Passionate about building websites that takes you on a unique experience.
+        </h5>
+    </section>
+  )
+}
+
+export default HomeTextRight
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HomeTextLeft from '../../components/homeTextLeft/HomeTextLeft'
+import HomeTextRight from '../../components/homeTextRight/HomeTextRight'
 import './Home.scss'
 
 const Home = () => {
   return (
     <div className="home__container">
-      <HomeTextLeft />
+      <HomeTextLeft/>
+      <HomeTextRight/>
     </div>
   )
 }

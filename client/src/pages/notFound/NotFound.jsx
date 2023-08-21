506df253da2ee21c983745bc528e5f0643e65e87
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-08-21 14:17:56 +0100

    notFound ready to push

--- client/src/pages/notFound/NotFound.jsx
+++ client/src/pages/notFound/NotFound.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaRobot } from 'react-icons/fa'
 import './NotFound.scss'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <section className="notFound__container">
+      <FaRobot/>
+      <h2 className="notFound__heading">
+        Oops...something went wrong!
+      </h2>
+      <h4 className="notFound__text">
+        Don't worry, you didn't break anything. Just head on back to the homepage.
+      </h4>
+      <Link to='/' className='notFound__link'>
+        <button className="notFound__homepage">
+          Go to Homepage
+        </button>        
+      </Link>
+    </section>
   )
 }
 

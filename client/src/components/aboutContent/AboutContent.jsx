a0460ef3bcf1be9312fcbca8e2b772bb436be72d
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2023-08-22 18:20:43 +0100

    aboutContent ready to push

---
+++ client/src/components/aboutContent/AboutContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutAside from '../../components/aboutAside/AboutAside'
+import AboutText from '../../components/aboutText/AboutText'
+
+const AboutContent = () => {
+  return (
+    <div className="about__content">
+        <AboutAside/>
+        <AboutText/>
+    </div>
+  )
+}
+
+export default AboutContent
--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import AboutHeading from '../../components/aboutHeading/AboutHeading'
-import AboutAside from '../../components/aboutAside/AboutAside'
-import AboutText from '../../components/aboutText/AboutText'
+import AboutContent from '../../components/aboutContent/AboutContent'
 import './About.scss'
 
 const About = () => {
   return (
     <section className="about__container">
       <AboutHeading/>
-      <div className="about__content">
-        <AboutAside/>
-        <AboutText/>
-      </div>
+      <AboutContent/>
     </section>
   )
 }
